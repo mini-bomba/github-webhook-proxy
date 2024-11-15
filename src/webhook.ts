29bb0a2b75c17f1e1cc286e8690e416146a9113b
mini-bomba <55105495+mini-bomba@users.noreply.github.com>
2024-11-15 23:13:51 +0100

    Improve pull request messages
    things done:
    - differentiate between closing a PR and merging it
    - send messages when converting a PR to draft and back
    - add embed colors

--- src/webhook.ts
+++ src/webhook.ts
@@ -1,5 +1,5 @@
 import { fetchResponse, textResponse } from "./util";
-import { IssuesEvent, ReleaseEvent } from "@octokit/webhooks-types";
+import { IssuesEvent, PullRequestEvent, ReleaseEvent } from "@octokit/webhooks-types";
 
 const webhook_regex = /^\/(\d+)\/([\w-_]+)\/?(?:github)?$/;
 
@@ -107,6 +107,73 @@ async function handleIssueEvent(request: Request, webhook_url: string): Promise<
     });
 }
 
+async function handlePREvent(request: Request, webhook_url: string): Promise<Response> {
+    let event: PullRequestEvent;
+    try {
+        event = await request.json();
+    } catch (e) {
+        let message = "";
+        if (typeof e === "string") {
+            message = e;
+        } else if (e instanceof Error) {
+            message = e.message;
+        }
+        return textResponse(`Error while parsing input JSON: ${message}`, 400);
+    }
+
+    let action: string;
+    let color: number;
+    switch(event.action) {
+        case "converted_to_draft":
+            action = "converted to draft";
+            color = 0x212830;
+            break;
+        case "ready_for_review":
+            action = "ready for review";
+            color = 0xf6f8fa;
+            break;
+        case "reopened":
+            action = "reopened";
+            color = 0x1f883d;
+            break;
+        case "closed":
+            if (event.pull_request.merged) {
+                action = "merged"
+                color = 0x8250df;
+            } else {
+                action = "closed";
+                color = 0xcf222e;
+            }
+            break;
+        // some other action type we don't want to modify
+        default:
+            return await fetchResponse(`${webhook_url}/github`, {
+                method: "POST",
+                headers: request.headers,
+                body: JSON.stringify(event),
+            });
+    }
+
+    return await fetchResponse(webhook_url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json;charset=UTF-8" },
+        body: JSON.stringify({
+            username: "GitHub",
+            avatar_url: "https://cdn.discordapp.com/attachments/743515515799994489/996513463650226327/unknown.png",
+            embeds: [{
+                author: {
+                    name: event.sender.login,
+                    url: event.sender.html_url,
+                    icon_url: event.sender.avatar_url,
+                },
+                title: `[${event.repository.full_name}] Pull request ${action}: #${event.pull_request.number} ${event.pull_request.title}`,
+                url: event.pull_request.html_url,
+                color,
+            }],
+        }),
+    });
+}
+
 export default async function webhook(request: Request): Promise<Response> {
     const url: URL = new URL(request.url);
     const [_, channel_id, webhook_token] = webhook_regex.exec(url.pathname) ?? [];
@@ -121,6 +188,8 @@ export default async function webhook(request: Request): Promise<Response> {
             return await handleReleaseEvent(request, webhook_url);
         case "issues":
             return await handleIssueEvent(request, webhook_url);
+        case "pull_request":
+            return await handlePREvent(request, webhook_url);
         default:
             return await fetchResponse(`${webhook_url}/github`, request);
     }
